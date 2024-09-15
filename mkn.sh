e771695711f3ed8bbd97613fd62d36b5891c1d17
PhilipDeegan <philip.deegan@gmail.com>
2024-09-15 21:10:43 +0200

    ++

--- mkn.sh
+++ mkn.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 set -exu
-CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" && cd $CWD
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 (
     cd $CWD
     mkn
-    rm -rf build && mkdir build && cd build
-    cmake -G Ninja -DCMAKE_BUILD_TYPE=Release \
-        -DBUILD_EXAMPLES=OFF \
-        -DCMAKE_INSTALL_PREFIX=$CWD ../f
-    ninja && ninja install
+    rm -rf build && mkdir build
+    (
+        cd build
+        cmake -G Ninja -DCMAKE_BUILD_TYPE=Release \
+            -DBUILD_EXAMPLES=OFF \
+            -DCMAKE_INSTALL_PREFIX=$CWD ../f
+        ninja && ninja install
+    )
+    rm -rf build
 )
